c936be48ebaf2f77cd10bfd231f119dac0aea6ee
deniskonnov21 <78259008+deniskonnov21@users.noreply.github.com>
2026-05-14 09:40:50 +0300

    Update PageService.java

--- src/main/java/com/example/watcher/PageService.java
+++ src/main/java/com/example/watcher/PageService.java
@@ -10,35 +10,35 @@ import java.util.List;
 
 @Service
 public class PageService {
-//    public String extractData(String url) throws Exception {
-//        Document doc = Jsoup.connect(url).get();
-//        Elements elements = doc.select("a");
-//        return elements.stream().map(e -> e.text()+"|"+e.attr("href")).reduce("", String::concat);
-//    }
+   public String extractData(String url) throws Exception {
+       Document doc = Jsoup.connect(url).get();
+       Elements elements = doc.select("a");
+       return elements.stream().map(e -> e.text()+"|"+e.attr("href")).reduce("", String::concat);
+   }
 
-    public List<Product> getProducts(String url) throws Exception {
+    // public List<Product> getProducts(String url) throws Exception {
 
-        Document doc = Jsoup.connect(url).get();
+    //     Document doc = Jsoup.connect(url).get();
 
-        List<Product> result = new ArrayList<>();
+    //     List<Product> result = new ArrayList<>();
 
-        Elements rows = doc.select("tr"); // или нужный selector
+    //     Elements rows = doc.select("tr"); // или нужный selector
 
-        for (Element row : rows) {
+    //     for (Element row : rows) {
 
-            Elements cols = row.select("td");
+    //         Elements cols = row.select("td");
 
-            if (cols.size() >= 2) {
-                String name = cols.get(0).text();
-                String price = cols.get(1).text();
+    //         if (cols.size() >= 2) {
+    //             String name = cols.get(0).text();
+    //             String price = cols.get(1).text();
 
-                if (!name.isEmpty()) {
-                    result.add(new Product(name, price));
-                }
-            }
-        }
+    //             if (!name.isEmpty()) {
+    //                 result.add(new Product(name, price));
+    //             }
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
 }
