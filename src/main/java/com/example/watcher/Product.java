a8db9add4508c5fdf1294d4545d14b7a97094d31
deniskonnov21 <78259008+deniskonnov21@users.noreply.github.com>
2026-05-14 09:42:16 +0300

    Update Product.java

--- src/main/java/com/example/watcher/Product.java
+++ src/main/java/com/example/watcher/Product.java
@@ -1,35 +1,35 @@
-package com.example.watcher;
+// package com.example.watcher;
 
-public class Product {
+// public class Product {
 
-    private String name;
-    private String price;
+//     private String name;
+//     private String price;
 
-    public Product(String name, String price) {
-        this.name = name;
-        this.price = price;
-    }
+//     public Product(String name, String price) {
+//         this.name = name;
+//         this.price = price;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public String getPrice() {
-        return price;
-    }
+//     public String getPrice() {
+//         return price;
+//     }
 
-    // equals/hashCode только по name (важно!)
+//     // equals/hashCode только по name (важно!)
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Product)) return false;
-        Product product = (Product) o;
-        return name.equals(product.name);
-    }
+//     @Override
+//     public boolean equals(Object o) {
+//         if (this == o) return true;
+//         if (!(o instanceof Product)) return false;
+//         Product product = (Product) o;
+//         return name.equals(product.name);
+//     }
 
-    @Override
-    public int hashCode() {
-        return name.hashCode();
-    }
-}
+//     @Override
+//     public int hashCode() {
+//         return name.hashCode();
+//     }
+// }
